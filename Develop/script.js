91008181266e01b9872b279a8094a6dc25e11fe9
Josh Thomas <josh061236@outlook.com>
2022-08-22 16:48:31 -0500

    password generator project updateds (HTML, CSS, JS)

--- Develop/script.js
+++ Develop/script.js
@@ -4,14 +4,53 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(length, lowercase, uppercase, numeric, specialcharacters) {
+  var password = '';
+  var str = '';
+  if (uppercase == "on"){
+    str += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  }
+  if (lowercase == "on"){
+      str += 'abcdefghijklmnopqrstuvwxyz';
+  }
+  if (numeric == "on"){
+      str += '0123456789';
+  }
+  if (specialcharacters == "on"){
+      str += '@#$';
+  }
+  for (let i = 1; i <= length; i++) {
+      var char = Math.floor(Math.random()
+                  * str.length + 1);
+        
+      password += str.charAt(char)
+  }
+    
+  return password;
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var form = document.querySelector("#constraints");
+var length = document.getElementById("length").value;
+var lowercase = document.getElementById("lowercase").value;
+var uppercase = document.getElementById("uppercase").value;
+var numeric = document.getElementById("numeric").value;
+var specialcharacters = document.getElementById("special-characters").value;
+checked = document.querySelectorAll("input[type=checkbox]:checked").length;
+console.log(checked);
+if(!checked) {
+  alert("You must check at least one checkbox.");
+  return false;
+}
+console.log(lowercase);
+  var password = generatePassword(length, lowercase, uppercase, numeric, specialcharacters);
+  console.log(password);
+  /* var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = password; */
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+ // Add event listener to generate button
+generateBtn.addEventListener("click", writePassword); 
